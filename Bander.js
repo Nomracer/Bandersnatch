7d5c65999ee5664faa29835a94fd4c1f854dee8b
Gürkan <gurkansimsek2153@gmail.com>
2023-03-22 22:19:52 +0300

    Deneme 1.1

--- Bander.js
+++ Bander.js
@@ -1,24 +1,40 @@
-var button1 = document.getElementById("button1");
+var buttonWrapper = document.getElementById("button-wrapper");
+		var button1 = document.getElementById("button1");
 		var button2 = document.getElementById("button2");
 		var video = document.getElementById("video");
-		var buttonWrapper = document.querySelector(".button-wrapper");
 
-    buttonWrapper.addEventListener("click", function() {
-			buttonWrapper.style.display = "none";
+		video.addEventListener("play", function() {
+			setTimeout(function() {
+				buttonWrapper.style.opacity = 1;
+			}, 3000);
+		});
+
+		buttonWrapper.addEventListener("click", function() {
+			buttonWrapper.style.opacity = 0;
+			setTimeout(function() {
+				buttonWrapper.style.display = "none";
+			}, 500);
 		});
 
 		button1.addEventListener("click", function() {
 			video.src = "soldrib.mp4";
 			video.play();
+			buttonWrapper.style.opacity = 0;
+			setTimeout(function() {
+				buttonWrapper.style.display = "none";
+			}, 500);
 		});
 
 		button2.addEventListener("click", function() {
 			video.src = "Sol.mp4";
 			video.play();
+			buttonWrapper.style.opacity = 0;
+			setTimeout(function() {
+				buttonWrapper.style.display = "none";
+			}, 500);
 		});
 
-		video.addEventListener("play", function() {
-			setTimeout(function() {
-				buttonWrapper.classList.add("hide");
-			}, 5000);
-		});
+		buttonWrapper.style.opacity = 0;
+		setTimeout(function() {
+			buttonWrapper.style.opacity = 1;
+		}, 3000);
