4c7417679aab12cedfc9b0bb03cb1906e5c84cad
Gürkan <gurkansimsek2153@gmail.com>
2023-04-10 16:39:20 +0300

    Demo 2.0

--- Bander.js
+++ Bander.js
@@ -1,30 +1,27 @@
 var buttonWrapper = document.getElementById("button-wrapper");
 var button1 = document.getElementById("button1");
-var button2 = document.getElementById("button2");
 var video = document.getElementById("video");
 
-		video.addEventListener("play", function() {
-			setTimeout(function() {
-				button1.style.opacity = 1;
-			}, 5000);
-		});
+	video.addEventListener("timeupdate", function() {
+		if (video.currentTime >= 5 && video.currentTime <= 9) {
+	  		button1.style.display = "inline-block";
+			}
+		 else {
+	  button1.style.display = "none";
+	}
+  });
 	
-		button1.addEventListener("click", function() {
-			button1.style.opacity = 0;
-			setTimeout(function() {
-				button1.style.display = "none";
-			}, 500);
-		});
+
+  button1.addEventListener("click", function() {
+	button1.style.display ="none";
+});
 
 		button1.addEventListener("click", function() {
 			video.currentTime = 210;
 		});
 
-		//button2.addEventListener("click", function(){
-		//	video.currentTime = 120;
-		//});
-
 		button1.style.opacity = 0;
 		setTimeout(function() {
 			button1.style.opacity = 1;
 		}, 5000);
+		
